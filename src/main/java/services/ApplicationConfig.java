000e471e0436a370a3d8f00d4e94187633e6288c
Puran Karki <xettripuran81@gmail.com>
2025-05-18 11:08:26 +0300

    Implement RobotService.java to handle REST API logic

---
+++ src/main/java/services/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package services;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+
+public class ApplicationConfig extends Application {
+}
