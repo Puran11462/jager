e5f5066f1094daa9ef28698ddae11e25352b03e5
Puran Karki <xettripuran81@gmail.com>
2025-05-18 11:12:12 +0300

    setup for the ev3 robot to connect with the server

---
+++ Ev3/src/package.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cvfe ev31.jar threads.MainEV3 -C ..\bin .
--- src/main/java/services/RobotService.java
+++ src/main/java/services/RobotService.java
@@ -1,3 +1,65 @@
+package services;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import data.Robot;
+
+@Path("/robotservice")
 public class RobotService {
     
+
+    @GET
+    @Path("/info")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String info() {
+        return "This is working...";
+    }
+
+    // Reading all robots from the table
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Robot> readAllRobot() {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("jager");
+        EntityManager em = emf.createEntityManager();
+        List<Robot> list = em.createQuery("select a from Robot a", Robot.class).getResultList();
+        return list;
+    }
+
+    // Adding one robot object into the table
+    @POST
+    @Path("/addrobot")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Robot postRobot(Robot robot) {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("jager");
+        EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(robot);
+            em.getTransaction().commit();
+            return robot;
+    }
+
+    // Fetch the latest robot based on highest ID (assuming ID is auto-incremented)
+    @GET
+    @Path("/latest")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Robot getLatestRobotSettings() {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("jager");
+        EntityManager em = emf.createEntityManager();
+        Robot latestRobot = em.createQuery("SELECT a FROM Robot a ORDER BY a.id DESC", Robot.class)
+                              .setMaxResults(1)
+                              .getSingleResult();
+        return latestRobot;
+    }
 }
